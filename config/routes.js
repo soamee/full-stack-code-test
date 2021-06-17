129ad4b634df4e780abecfdaddc5f2fedebca6c4
Eduardo García <edugarciasb16@gmail.com>
2021-06-17 03:46:09 +0200

    routes config

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const BookController = require('../controllers/book.controller')
 
+//endpoints
 router.get('/books', BookController.getBooks)
 
 module.exports = router
